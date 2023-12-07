fb841be42a33a649ec2b36ffc5baaab83d18fc00
Matheus4ugusto <matheuspaulasoares10@gmail.com>
2023-12-06 21:45:52 -0300

    Adiciona estruturas de rotas da aplicação

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import routesV1 from "./routes/routesV1";
 //cors é uma função/classe que fica entre a requisição e a controller, "é o porteiro da aplicação"-middleware
 const app = express();
 
@@ -9,12 +10,19 @@ app.use(
   })
 );
 
+app.use("/api/v1", routesV1);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 app.use((req, res) => {
   res.status(404);
+  res.send("Rota não encontrada!");
 });
 
 app.listen(process.env.PORT, () => {
   console.log(
-    `Servidor executando em: ${process.env.HOST} : ${process.env.PORT}`
+    `Servidor executando em: ${process.env.HOST}: ${process.env.PORT}`
   ); //sempre que for acessar algo presente no .env, usar process.env.[o que for desejado]
 });
