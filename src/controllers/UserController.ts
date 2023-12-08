091e2e966c3caac9a2a1dcde8cff8ec2a52189a6
Matheus4ugusto <matheuspaulasoares10@gmail.com>
2023-12-07 22:08:26 -0300

    Implementa controller de usuários

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+
+export class UserController {
+  constructor() {}
+
+  async getUsers(request: Request, responde: Response): Response {}
+  async getUser(request: Request, Response: Response): Response {}
+  async saveUser(request: Request, response: Response): Response {}
+}
---
+++ src/services/UserService.ts
@@ -0,0 +1,71 @@
+import { PrismaClient, User } from "@prisma/client";
+import { PasswordEncoder } from "./PasswordEncoder";
+import { NotFoundException } from "../exceptions/NotFoundExceptions";
+
+interface StoreUser extends Omit<User, "id" | "createdAt" | "updatedAt"> {}
+interface UpdateUser extends Partial<User> {}
+
+export class UserService {
+  private readonly PasswordEncoder: PasswordEncoder;
+  private readonly prisma: PrismaClient;
+
+  constructor() {
+    this.PasswordEncoder = new PasswordEncoder();
+    this.prisma = new PrismaClient();
+  }
+
+  async getUsers(): Promise<User[]> {
+    return this.prisma.user.findMany();
+  }
+
+  async createUser(data: StoreUser) {
+    data.password = await this.PasswordEncoder.encode(data.password);
+    return this.prisma.user.create({ data });
+  }
+
+  async getUserById(id: string): Promise<User> {
+    const user: User | null = await this.prisma.user.findFirst({
+      where: {
+        id,
+      },
+    });
+    if (!user) {
+      throw new NotFoundException("Usuário não encontrado!");
+    }
+
+    return user;
+  }
+
+  async getUserByEmail(email: string): Promise<User> {
+    const user: User | null = await this.prisma.user.findFirst({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundException("Usuário não encontrado.");
+    }
+
+    return user;
+  }
+
+  async updateUser(id: string, data: UpdateUser): Promise<User> {
+    const user: User = await this.prisma.user.update({
+      where: {
+        id,
+      },
+      data,
+    });
+
+    return user;
+  }
+
+  async deleteUser(id: string): Promise<void> {
+    await this.prisma.user.delete({
+      where: {
+        id,
+      },
+    });
+  }
+}
